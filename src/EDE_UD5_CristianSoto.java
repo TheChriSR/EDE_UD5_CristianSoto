faed44a01bfb27d3cddd71d4f68bad8fc1d006f3
Sergio Sánchez Martínez <sersanmar9@alu.edu.gva.es>
2023-12-03 21:35:36 +0100

    Segunda_version

--- src/EDE_UD5_CristianSoto.java
+++ src/EDE_UD5_CristianSoto.java
@@ -1,7 +1,9 @@
 public class EDE_UD5_CristianSoto
 {
     public static void main(String[] args)
+
     {
         System.out.println("Hola, ¿que tal?");
+        System.out.println("Versión modificada por Sergio Sánchez Martínez");
     }
 }
